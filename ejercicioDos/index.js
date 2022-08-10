03d3c47e6e017099205fa1fa86bbb6542f4f785e
Efrael <efrael2001>
2022-08-10 14:27:53 -0500

    Ejercicios dos terminado

---
+++ ejercicioDos/index.js
@@ -0,0 +1,5 @@
+const me=["Efrael", 20, true, 11-10-2001,
+["Rompe las barreras del No", "Chriss Boss", 23-06-2022, "https://librosgratisparaleer.com/rompe-la-barrera-del-no-chris-voss-pdf/"]
+
+]
+console.log(me)
