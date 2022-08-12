fd1e99e14995c4c0dd356595d511beb4e99b3c7f
Efrael <efrael2001>
2022-08-12 09:18:46 -0500

    ejercicio cinco terminado

---
+++ ejercicioCinco/index.js
@@ -0,0 +1,12 @@
+let myTalla= 165
+let myTallaF=1.6500
+let myPeso=65.052
+
+let myTalla_Max= myTallaF.MIN_VALUE
+console.log(myTallaF)
+
+let myPeso_Min=Math.floor(myPeso)
+console.log(myPeso_Min)
+
+let compracion =(myTallaF == myPeso_Min)
+console.log(compracion)
