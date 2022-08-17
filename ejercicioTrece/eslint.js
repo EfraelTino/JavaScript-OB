02d047bc77a2890f931da7c1e9dc2b054e8ef7af
Efrael <efrael2001>
2022-08-17 13:51:41 -0500

    ejercicio trece terminado

---
+++ ejercicioTrece/eslint.js
@@ -0,0 +1,11 @@
+let str_dbl= "Hola soy un string";
+let str_sgn= "Hola soy un string con comillas simple";
+
+console.log(str_dbl);
+console.log(str_sgn);
+
+let str_comillas= "Hola soy un string entre comilla";
+
+console.log(str_comillas);
+
+
